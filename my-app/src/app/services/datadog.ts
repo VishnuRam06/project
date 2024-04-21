8edb1d9debf5f836a0e47ab403a61a0255d36516
VishnuRam06 <118251533+VishnuRam06@users.noreply.github.com>
2024-04-21 13:40:55 +0530

    Update datadog.ts

--- my-app/src/app/services/datadog.ts
+++ my-app/src/app/services/datadog.ts
@@ -2,8 +2,8 @@ import { client, v1 } from "@datadog/datadog-api-client";
 
 const configuration = client.createConfiguration({
   authMethods: {
-    apiKeyAuth: "768c73d89899580e3471e4825f165a32",
-    appKeyAuth: "238348ba19aec7d7cee10a1a709d47fe0ea88aba",
+    apiKeyAuth: "94023f347d96a0d36d32c73902da1daa",
+    appKeyAuth: "5bdf31b912f33f2744cd6b28f08174c4bf9d7563",
   },
 });
 
